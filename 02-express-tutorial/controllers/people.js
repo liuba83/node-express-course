5e86644d2fcc2bd319da381b8aa9863255df75a2
Liuba <liubabarusch@gmail.com>
2025-04-17 15:14:08 -0500

    add method to delete person

--- 02-express-tutorial/controllers/people.js
+++ 02-express-tutorial/controllers/people.js
@@ -21,4 +21,17 @@ const getPerson = (req, res) => {
   res.json(person);
 };
 
-module.exports = { addPerson, getPeople, getPerson };
+const deletePerson = (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, message: `no person with id ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  );
+  return res.status(200).json({ success: true, data: newPeople });
+};
+
+module.exports = { addPerson, getPeople, getPerson, deletePerson };
